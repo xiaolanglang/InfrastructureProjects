a1d42207621e385fd704c9d441b811f296c8bb0d
meikalong <meikalong>
2016-01-27 09:42:35 +0800

    时间工具中加入部分内容
    Signed-off-by: meikalong <meikalong>

--- src/com/bkweb/common/utils/DateUtils.java
+++ src/com/bkweb/common/utils/DateUtils.java
@@ -4,21 +4,25 @@
 package com.bkweb.common.utils;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import org.apache.commons.lang3.time.DateFormatUtils;
 
 /**
  * 日期工具类, 继承org.apache.commons.lang.time.DateUtils类
+ * 
  * @author ThinkGem
  * @version 2014-4-15
  */
 public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
-	
-	private static String[] parsePatterns = {
-		"yyyy-MM-dd", "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd HH:mm", "yyyy-MM", 
-		"yyyy/MM/dd", "yyyy/MM/dd HH:mm:ss", "yyyy/MM/dd HH:mm", "yyyy/MM",
-		"yyyy.MM.dd", "yyyy.MM.dd HH:mm:ss", "yyyy.MM.dd HH:mm", "yyyy.MM"};
+
+	private static String[] parsePatterns = { "yyyy-MM-dd", "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd HH:mm", "yyyy-MM",
+			"yyyy/MM/dd", "yyyy/MM/dd HH:mm:ss", "yyyy/MM/dd HH:mm", "yyyy/MM", "yyyy.MM.dd", "yyyy.MM.dd HH:mm:ss",
+			"yyyy.MM.dd HH:mm", "yyyy.MM" };
 
 	/**
 	 * 得到当前日期字符串 格式（yyyy-MM-dd）
@@ -26,14 +30,14 @@ public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
 	public static String getDate() {
 		return getDate("yyyy-MM-dd");
 	}
-	
+
 	/**
 	 * 得到当前日期字符串 格式（yyyy-MM-dd） pattern可以为："yyyy-MM-dd" "HH:mm:ss" "E"
 	 */
 	public static String getDate(String pattern) {
 		return DateFormatUtils.format(new Date(), pattern);
 	}
-	
+
 	/**
 	 * 得到日期字符串 默认格式（yyyy-MM-dd） pattern可以为："yyyy-MM-dd" "HH:mm:ss" "E"
 	 */
@@ -46,7 +50,7 @@ public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
 		}
 		return formatDate;
 	}
-	
+
 	/**
 	 * 得到日期时间字符串，转换格式（yyyy-MM-dd HH:mm:ss）
 	 */
@@ -95,15 +99,14 @@ public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
 	public static String getWeek() {
 		return formatDate(new Date(), "E");
 	}
-	
+
 	/**
-	 * 日期型字符串转化为日期 格式
-	 * { "yyyy-MM-dd", "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd HH:mm", 
-	 *   "yyyy/MM/dd", "yyyy/MM/dd HH:mm:ss", "yyyy/MM/dd HH:mm",
-	 *   "yyyy.MM.dd", "yyyy.MM.dd HH:mm:ss", "yyyy.MM.dd HH:mm" }
+	 * 日期型字符串转化为日期 格式 { "yyyy-MM-dd", "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd HH:mm",
+	 * "yyyy/MM/dd", "yyyy/MM/dd HH:mm:ss", "yyyy/MM/dd HH:mm", "yyyy.MM.dd",
+	 * "yyyy.MM.dd HH:mm:ss", "yyyy.MM.dd HH:mm" }
 	 */
 	public static Date parseDate(Object str) {
-		if (str == null){
+		if (str == null) {
 			return null;
 		}
 		try {
@@ -115,48 +118,52 @@ public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
 
 	/**
 	 * 获取过去的天数
+	 * 
 	 * @param date
 	 * @return
 	 */
 	public static long pastDays(Date date) {
-		long t = new Date().getTime()-date.getTime();
-		return t/(24*60*60*1000);
+		long t = new Date().getTime() - date.getTime();
+		return t / (24 * 60 * 60 * 1000);
 	}
 
 	/**
 	 * 获取过去的小时
+	 * 
 	 * @param date
 	 * @return
 	 */
 	public static long pastHour(Date date) {
-		long t = new Date().getTime()-date.getTime();
-		return t/(60*60*1000);
+		long t = new Date().getTime() - date.getTime();
+		return t / (60 * 60 * 1000);
 	}
-	
+
 	/**
 	 * 获取过去的分钟
+	 * 
 	 * @param date
 	 * @return
 	 */
 	public static long pastMinutes(Date date) {
-		long t = new Date().getTime()-date.getTime();
-		return t/(60*1000);
+		long t = new Date().getTime() - date.getTime();
+		return t / (60 * 1000);
 	}
-	
+
 	/**
 	 * 转换为时间（天,时:分:秒.毫秒）
+	 * 
 	 * @param timeMillis
 	 * @return
 	 */
-    public static String formatDateTime(long timeMillis){
-		long day = timeMillis/(24*60*60*1000);
-		long hour = (timeMillis/(60*60*1000)-day*24);
-		long min = ((timeMillis/(60*1000))-day*24*60-hour*60);
-		long s = (timeMillis/1000-day*24*60*60-hour*60*60-min*60);
-		long sss = (timeMillis-day*24*60*60*1000-hour*60*60*1000-min*60*1000-s*1000);
-		return (day>0?day+",":"")+hour+":"+min+":"+s+"."+sss;
-    }
-	
+	public static String formatDateTime(long timeMillis) {
+		long day = timeMillis / (24 * 60 * 60 * 1000);
+		long hour = (timeMillis / (60 * 60 * 1000) - day * 24);
+		long min = ((timeMillis / (60 * 1000)) - day * 24 * 60 - hour * 60);
+		long s = (timeMillis / 1000 - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60);
+		long sss = (timeMillis - day * 24 * 60 * 60 * 1000 - hour * 60 * 60 * 1000 - min * 60 * 1000 - s * 1000);
+		return (day > 0 ? day + "," : "") + hour + ":" + min + ":" + s + "." + sss;
+	}
+
 	/**
 	 * 获取两个日期之间的天数
 	 * 
@@ -169,15 +176,128 @@ public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
 		long afterTime = after.getTime();
 		return (afterTime - beforeTime) / (1000 * 60 * 60 * 24);
 	}
-	
+
 	/**
-	 * @param args
-	 * @throws ParseException
+	 * 根据某一天获得当天所在的一周
+	 * 
+	 * @param mdate
+	 * @return
 	 */
-	public static void main(String[] args) throws ParseException {
-//		System.out.println(formatDate(parseDate("2010/3/6")));
-//		System.out.println(getDate("yyyy年MM月dd日 E"));
-//		long time = new Date().getTime()-parseDate("2012-11-19").getTime();
-//		System.out.println(time/(24*60*60*1000));
+	@SuppressWarnings("deprecation")
+	public static List<Date> dateToWeek(Date mdate) {
+		int b = mdate.getDay();
+		Date fdate;
+		List<Date> list = new ArrayList<Date>();
+		Long fTime = mdate.getTime() - b * 24 * 3600000;
+		for (int a = 0; a < 7; a++) {
+			fdate = new Date();
+			fdate.setTime(fTime + ((a + 1) * 24 * 3600000));
+			list.add(a, fdate);
+		}
+
+		return list;
 	}
+
+	/**
+	 * 根据某一天获得当天所在的一周
+	 * 
+	 * @param date
+	 *            要求日期格式是：yyyy-MM-dd
+	 * @return
+	 */
+	public static List<Date> dateToWeek(String date) {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		List<Date> list = null;
+		try {
+			list = dateToWeek(format.parse(date));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	/**
+	 * 把一个日期集合变成日期字符串集合
+	 * 
+	 * @param list
+	 * @return 集合中的字符串格式是：yyyy-MM-dd
+	 */
+	public static List<String> dateToString(List<Date> list) {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		List<String> dates = new ArrayList<String>();
+		for (Date date : list) {
+			dates.add(format.format(date));
+		}
+		return dates;
+	}
+
+	/**
+	 * 根据某一天获得当天所在的一个月
+	 * 
+	 * @param date
+	 *            要求日期格式是：yyyy-MM-dd
+	 * @return
+	 */
+	public static List<String> dateToMonth(String date) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date d = null;
+		try {
+			d = sdf.parse(date);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		Date monthStart = getMonthStart(d);
+		Date monthEnd = getMonthEnd(d);
+		List<String> list = new ArrayList<String>();
+		while (!monthStart.after(monthEnd)) {
+			list.add(sdf.format(monthStart));
+			monthStart = getNext(monthStart);
+		}
+
+		return list;
+
+	}
+
+	/**
+	 * 获得一个月的开始
+	 * 
+	 * @param date
+	 * @return
+	 */
+	private static Date getMonthStart(Date date) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		int index = calendar.get(Calendar.DAY_OF_MONTH);
+		calendar.add(Calendar.DATE, (1 - index));
+		return calendar.getTime();
+	}
+
+	/**
+	 * 获得一个月的数据
+	 * 
+	 * @param date
+	 * @return
+	 */
+	private static Date getMonthEnd(Date date) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		calendar.add(Calendar.MONTH, 1);
+		int index = calendar.get(Calendar.DAY_OF_MONTH);
+		calendar.add(Calendar.DATE, (-index));
+		return calendar.getTime();
+	}
+
+	/**
+	 * 下一天
+	 * 
+	 * @param date
+	 * @return
+	 */
+	private static Date getNext(Date date) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		calendar.add(Calendar.DATE, 1);
+		return calendar.getTime();
+	}
+
 }
