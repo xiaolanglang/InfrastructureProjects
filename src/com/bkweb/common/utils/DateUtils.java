1a4e0dc8bd225ca62d59d010001026a4e0bd2881
meikalong <meikalong>
2016-02-02 16:33:32 +0800

    Signed-off-by: meikalong <meikalong>

--- src/com/bkweb/common/utils/DateUtils.java
+++ src/com/bkweb/common/utils/DateUtils.java
@@ -1,6 +1,3 @@
-/**
- * Copyright &copy; 2012-2014 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
- */
 package com.bkweb.common.utils;
 
 import java.text.ParseException;
